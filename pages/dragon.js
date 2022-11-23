034259d924ffc0dd1749de065d5fe53db3171782
Gamabears <871400768@qq.com>
2022-11-22 23:55:27 -0800

    Finshed

---
+++ pages/dragon.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import CountUp from 'react-countup';
+
+
+function dragon() {
+  return (
+    <>
+    <Header />
+    <section className='section-animate bg-dragon'></section>
+    <div className='section-inner-center'>
+        <h3>Dragon</h3>
+        <p>Sending humans and cargo into space</p>
+    </div>
+
+    <div className='stats'>
+        <div>
+            <span className='counter'>
+                <CountUp start = {0} end = {34} duartion = {1.5} />
+                
+            </span>
+            <h4>Total Launches</h4>
+        </div>
+        <div>
+            <span className='counter'><CountUp start = {0} end = {31} duartion = {1.5} /></span>
+            <h4>Visits to the ISS</h4>
+        </div>
+        <div>
+            <span className='counter'><CountUp start = {0} end = {13} duartion = {1.5} /></span>
+            <h4>Reflow Missions</h4>
+        </div>
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+export default dragon
---
+++ pages/falcon9.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import CountUp from 'react-countup';
+
+
+function falcon9() {
+  return (
+    <>
+    <Header />
+    <section className='section-animate bg-falcon-9'></section>
+    <div className='section-inner-center'>
+        <h3>Falcon 9</h3>
+        <p>First orbital class rocket capable of reflight</p>
+    </div>
+
+    <div className='stats'>
+        <div>
+            <span className='counter'><CountUp start = {0} end = {3} duartion = {1} /></span>
+            <h4>Total Launches</h4>
+        </div>
+        <div>
+            <span className='counter'><CountUp start = {0} end = {7} duartion = {1} /></span>
+            <h4>Total Landings</h4>
+        </div>
+        <div>
+            <span className='counter'><CountUp start = {0} end = {4} duartion = {1} /></span>
+            <h4>Total Reflights</h4>
+        </div>
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+export default falcon9
